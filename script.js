b99862288bb8d2c89ad31040106a716c7e4d7d34
george lucas <glalmeida.dev@gmail.com>
2021-10-26 16:58:18 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const takeOl = document.querySelector('#lista-tarefas');
+const buttonCriarTarefa = document.querySelector('#criar-tarefa');
+const textoTarefasValue = document.querySelector('#texto-tarefa');
 
+function criaTarefa (){   
+    let li = document.createElement('li');
+    li.innerText = textoTarefasValue.value;
+    takeOl.appendChild(li);
+    textoTarefasValue.value = "";
+}
+function addEventoButton (){
+    buttonCriarTarefa.addEventListener('click', criaTarefa)
+};
+addEventoButton();
