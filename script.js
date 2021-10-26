db9289765251cdd25dd4f77d62a42d9210cfbf2d
george lucas <glalmeida.dev@gmail.com>
2021-10-26 19:06:30 -0300

    requisito 7

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ const textoTarefasValue = document.querySelector('#texto-tarefa');
 
 function criaTarefa (){   
     let li = document.createElement('li');
+    takeOl.addEventListener('click', colocaBackGround);
     li.innerText = textoTarefasValue.value;
     takeOl.appendChild(li);
     textoTarefasValue.value = "";
 }
+function colocaBackGround(evento){
+    evento.target.classList.add('selected');
+}
+
 function addEventoButton (){
     buttonCriarTarefa.addEventListener('click', criaTarefa)
 };
 addEventoButton();
-//comentário
+
+function addEventoALista (){
+    let takeOlArray = document.querySelector('#lista-tarefas');
+    for (let index = 0 ; index < takeOlArray.length; index =+1){
+        console.log(takeOlArray[index]);
+    }    
+}
+addEventoALista();
+//adicionar evento ao pai 
